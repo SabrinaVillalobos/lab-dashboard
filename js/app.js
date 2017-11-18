7018cdd5845293659093f2ed46a59208bda16b74
Wafflo <wafflish@gmail.com>
2017-11-18 13:28:46 -0300

    navbar shrink working

--- js/app.js
+++ js/app.js
@@ -9,11 +9,43 @@ console.log(data);
 
 //funcion que achica la barra de navegacion secundaria
 var iconav = document.getElementById("iconbar");
+var iconavs = document.getElementById("iconbars");
 var navbar = document.getElementById("sidebar");
 var userimg = document.getElementById("userpic");
 var iconsbar = document.getElementById("navbuttons");
-addEventListener("click", function(){
+var iconsede = document.getElementById("icon1");
+var icconspad = document.getElementsByClassName("fontico");
+var labico = document.getElementById("labico");
+var labicos = document.getElementById("labicos");
+var iconscontainer = document.getElementsByClassName("letterico");
+labicos.style.display="none";
+iconavs.style.display="none";
+iconav.addEventListener("click", function(){
+	document.getElementById("username").style.fontSize="13px";
 	navbar.style.width = "60px";
 	userimg.style.width = "47px";
+	iconsede.style.paddingLeft= "5px";
+	labico.style.display="none";
+	labicos.style.display= "block";
+	iconavs.style.display="block";
+	iconav.style.display="none";
+
+
+
+	 var ele = document.getElementsByClassName("letterico");
+for (var i = 0; i < ele.length; i++ ) {
+    ele[i].style.display="none";
+}
+
+	 var icopa = document.getElementsByClassName("fontico");
+for (var i = 0; i < icopa.length; i++ ) {
+    icopa[i].style.paddingLeft="4px";
+
+}
+
 });
 
+//vuelve la barra a la normalidad
+iconavs.addEventListener("click", function(){
+	window.location.reload(false);
+});
