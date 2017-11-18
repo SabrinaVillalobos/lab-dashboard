7a249d1a5cda90175894aea12e4bfe22cd5d2ba8
Wafflo <wafflish@gmail.com>
2017-11-18 01:19:44 -0300

    adding navbar function

--- js/app.js
+++ js/app.js
@@ -4,3 +4,16 @@
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
+
+
+
+//funcion que achica la barra de navegacion secundaria
+var iconav = document.getElementById("iconbar");
+var navbar = document.getElementById("sidebar");
+var userimg = document.getElementById("userpic");
+var iconsbar = document.getElementById("navbuttons");
+addEventListener("click", function(){
+	navbar.style.width = "60px";
+	userimg.style.width = "47px";
+});
+
