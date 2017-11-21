2f77b34873c5a4a84b49fcb0e05fa0f1cd09bdde
SabrinaV <31784893+SabrinaVillalobos@users.noreply.github.com>
2017-11-21 04:06:13 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,8 +1,50 @@
 /*
  * Funcionalidad de tu producto
  */
+
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
+
+//inicio funciones alumnas scl2017II
+//alumnas inscritas SCL 2017 II (enrollment), da 59
+function enrollment(gen){
+    var studentsnumber = gen.students.length;
+    return studentsnumber;
+}
+var totalStudSCL20172 = data.SCL["2017-2"];// cambiando esta parte de la data data.SCL["2017-2"]; la formula serviría para todas las sedes, no se integraron las demas por falta de tiempo
+totalStudSCL20172 = enrollment(totalStudSCL20172);
+console.log("cantidad de estudiantes SCL 2017 II" + " " + "=" + " " + ((totalStudSCL20172) - 2));//menos 2 poque hay un 2 arrays vacío
+
+//alumnas activas SCL2017, 26 activas en total, cambiando -data.SCL["2017-2"]- sirve para todas las sedes
+function activeStu(gen){
+    var activeS = 0;
+    for (var i = 0 ; i < gen.students.length ; i++){
+        if (gen.students[i].active == true){
+            activeS++;
+        }
+    }
+    return activeS;
+}
+
+var actSCL20172 = data.SCL["2017-2"];
+actSCL20172 = activeStu(actSCL20172);
+console.log("cantidad de estudiantes activas SCL 2017 II" + " " + "=" + " " +(actSCL20172));
+
+
+//alumnas inactivas SCL 2017 II (dropout) total 33 inactivas
+function dropoutStu (studentsnumber,activeS){ //(inscritas - activas = inactivas)
+    var drops = ((studentsnumber - activeS) - 2); //el -2 es por los 2 array vacios
+    return drops;
+}
+
+var dropoutSCL20172 = dropoutStu(totalStudSCL20172, actSCL20172);
+console.log("cantidad de estudiantes desertoras SCL 2017 II" + " " + "=" + " " + (dropoutSCL20172));
+
+// % de deserción
+
+var dropPercent = Math.round((dropoutSCL20172/totalStudSCL20172)*100);
+ console.log("porcentaje de estudiantes desertoras SCL 2017 II" + " " + "=" + " " + (Math.round((dropoutSCL20172/totalStudSCL20172)*100)));
+//fin funciones alumnas scl2017II
 
 //grafico enrolled
 // for para iterar toda la data
@@ -30,6 +72,69 @@ for (var i = 0; i < data.SCL['2017-2'].students.length; i++){
 // console.log (all);
 
 
+//funcion que achica la barra de navegacion secundaria
+var iconav = document.getElementById("iconbar");
+var iconavs = document.getElementById("iconbars");
+var navbar = document.getElementById("sidebar");
+var userimg = document.getElementById("userpic");
+var iconsbar = document.getElementById("navbuttons");
+var iconsede = document.getElementById("icon1");
+var icconspad = document.getElementsByClassName("fontico");
+var labico = document.getElementById("labico");
+var labicos = document.getElementById("labicos");
+var downico = document.getElementById("downico");
+var graphscontainer = document.getElementById("contgraphs");
+var iconscontainer = document.getElementsByClassName("letterico");
+labicos.style.display="none";
+iconavs.style.display="none";
+iconav.addEventListener("click", function(){
+	document.getElementById("username").style.fontSize="13px";
+	navbar.style.width = "60px";
+	userimg.style.width = "47px";
+	iconsede.style.paddingLeft= "5px";
+	labico.style.display="none";
+	labicos.style.display= "block";
+	iconavs.style.display="block";
+	iconav.style.display="none";
+	graphscontainer.style.left="60px";
+
+
+
+	 var ele = document.getElementsByClassName("letterico");
+for (var i = 0; i < ele.length; i++ ) {
+    ele[i].style.display="none";
+}
+
+	 var icopa = document.getElementsByClassName("fontico");
+for (var i = 0; i < icopa.length; i++ ) {
+    icopa[i].style.paddingLeft="4px";
+
+}
+
+});
+
+//vuelve la barra a la normalidad
+iconavs.addEventListener("click", function(){
+	window.location.reload(false);
+});
+
+//dropdown de paises al hacer click
+var chile = document.getElementById("chile")
+chile.addEventListener("click", function() {
+    document.getElementById("myDropdownp").classList.toggle("show");
+});
+
+//dropdown de año grupo y turno abre al hacer click
+downico.addEventListener("click", function() {
+    document.getElementById("myDropdown").classList.toggle("show");
+});
+
+//dropdown cambiar idioma
+var language = document.getElementById("language")
+language.addEventListener("click", function() {
+    document.getElementById("myDropdownl").classList.toggle("show");
+});
+
 var totalstudents = document.getElementById('graph1')
 var parrafo = document.createElement('p');
 var texParrafo = document.createTextNode('61 Students');
@@ -53,7 +158,6 @@ graph3.appendChild(parrafo3);
 var canvas = document.getElementById('graph4');
 var data = {
     labels: ["# Currently enrolled", "% Dropout"],
-<<<<<<< HEAD
     datasets: [
         {
             label: "Students",
@@ -65,23 +169,11 @@ var data = {
             data: [enrolled, retired],
         }
     ]
-=======
-    datasets: [{
-        label: "Students",
-        backgroundColor: "#FFC107",
-        borderColor: "#CC9B08",
-        borderWidth: 2,
-        hoverBackgroundColor: "#CA9A09",
-        hoverBorderColor: "#715604",
-        data: [59, 60],
-    }]
->>>>>>> 9e64cd53bdd307b261ca7a49a8f524abd9e81193
 };
 
 
 var option = {
     scales: {
-<<<<<<< HEAD
     yAxes:[{
             stacked:true,
         gridLines: {
@@ -201,9 +293,22 @@ var barChart2 = new Chart(graph7, {
         }
     }
 });
-=======
-        
 
->>>>>>> 9e64cd53bdd307b261ca7a49a8f524abd9e81193
+//muestra el home con los graficos
+var homebtn = document.getElementById("home");
+var hidestid = document.getElementById("studentscontainer");
+homebtn.addEventListener("click", function() {
+    graphscontainer.style.display = "block";
+    hidestid.style.display = "none";
+
+});
+
+//oculta el home para mostrar las estudintes
+var studentsbtn = document.getElementById("students");
+studentsbtn.addEventListener("click", function() {
+    graphscontainer.style.display = "none";
+    studentscontainer.style.display = "block";
+
+
 
 });
